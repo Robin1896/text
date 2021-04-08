04295534c0e2853cd411d4f6252b140a6bc6a544
robin zegers <robinzegers98@icloud.com>
2021-04-08 10:43:22 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './logo/components/index';
+
+class App extends React.Component {
+
+render() {
+  return (
+    <Logo />
+  )};
+
+};
+
+export default App;
